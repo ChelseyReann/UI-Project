1cb27e9eca9e38bc666f8d8ea1570f3655bbac47
ChelseyReann <chelseyreann@gmail.com>
2023-03-20 15:36:36 -0400

    adding event listeners for recipe 1 and recipe 2

--- script.js
+++ script.js
@@ -27,5 +27,49 @@ fetch(url, options)
         let recipe5 = document.querySelector("#recipe5")
         recipe5.innerText = recipe5name
         
-        console.log()
+        recipe1.addEventListener("click", ()=>{
+            let h1 = document.querySelector("h1")
+            h1.remove()
+            let h2 = document.querySelector(".recipeName")
+            h2.innerText = recipe1name
+            h2.style.textAlign = "center"
+            h2.style.textDecoration = "underline"
+            let img = document.querySelector(".recipeImg")
+            let recipe1image = res.results[0].thumbnail_url
+            img.src = recipe1image
+            let description = document.querySelector(".recipeDescription")
+            let description1 = res.results[0].description
+            description.innerText = description1
+            description.style.textAlign = "center"
+            description.style.marginTop = "10px"
+            let instructions = document.querySelector(".recipeInstruct")
+            let instructions1 = []
+            console.log(res.results[0].instructions[0].display_text)
+            // let p = document.createElement("p")
+            // p.innerText = recipe1name
+            // console.log(recipe1name)
+            // p.style.textAlign = "center"
+            // p.style.marginTop = "30%"
+            // p.style.textDecoration = "underline"
+            // p.style.color = "#1a1818"
+
+        })
+
+        recipe2.addEventListener("click", ()=>{
+            let h1 = document.querySelector("h1")
+            h1.remove()
+            let h2 = document.querySelector(".recipeName")
+            h2.innerText = recipe2name
+            h2.style.textAlign = "center"
+            h2.style.textDecoration = "underline"
+            let img = document.querySelector(".recipeImg")
+            let recipe1image = res.results[1].thumbnail_url
+            img.src = recipe1image
+            let description = document.querySelector(".recipeDescription")
+            let description1 = res.results[1].description
+            description.innerText = description1
+            description.style.textAlign = "center"
+            description.style.marginTop = "10px"
+        })
+        console.log(res)
     })
